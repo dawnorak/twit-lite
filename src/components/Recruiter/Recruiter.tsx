052589059101526b2bb7c5f7bcacc6749b1c57e1
dawnorak <sathi.vaigarai_2024@woxsen.edu.in>
2023-11-08 11:20:35 +0530

    changes

---
+++ src/components/Recruiter/Recruiter.tsx
@@ -0,0 +1,10 @@
+import netherlandsRecruiter from './jeremy-akeze-doghouse-it-recruitment.jpg';
+import * as S from './style';
+
+const Recruiter = () => (
+  <S.Container>
+
+  </S.Container>
+);
+
+export default Recruiter;
